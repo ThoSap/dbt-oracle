a2184c12a361d3101451c87741f935a40fa460fb
Thomas Sapelza <thomas.sapelza@gknpm.com>
2022-05-10 16:29:41 +0200

    Quote and uppercase merge

--- dbt/include/oracle/macros/materializations/incremental/helpers.sql
+++ dbt/include/oracle/macros/materializations/incremental/helpers.sql
@@ -16,32 +16,32 @@
 #}
 {% macro oracle_incremental_upsert_backup(tmp_relation, target_relation, unique_key=none, statement_name="main") %}
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
-    {%- set dest_cols_csv = dest_columns | map(attribute='name') | join(', ') -%}
+    {%- set dest_cols_csv = dest_columns | map(attribute='name').quote(schema=True, identifier=True) | upper | join(', ') -%}
 
     {%- if unique_key is not none -%}
     delete
-    from {{ target_relation }}
-    where ({{ unique_key }}) in (
-        select ({{ unique_key }})
-        from {{ tmp_relation }}
+    from {{ target_relation.quote(schema=True, identifier=True) | upper }}
+    where ({{ unique_key.quote(schema=True, identifier=True) | upper }}) in (
+        select ({{ unique_key.quote(schema=True, identifier=True) | upper }})
+        from {{ tmp_relation.quote(schema=True, identifier=True) | upper }}
     );
     {%- endif %}
 
-    insert into {{ target_relation }} ({{ dest_cols_csv }})
+    insert into {{ target_relation.quote(schema=True, identifier=True) | upper }} ({{ dest_cols_csv }})
     (
        select {{ dest_cols_csv }}
-       from {{ tmp_relation }}
+       from {{ tmp_relation.quote(schema=True, identifier=True) | upper }}
     )
 {%- endmacro %}
 
 {% macro oracle_incremental_upsert(tmp_relation, target_relation, unique_key=none, statement_name="main") %}
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
-    {%- set dest_cols_csv = dest_columns | map(attribute='name') | join(', ') -%}
+    {%- set dest_cols_csv = dest_columns | map(attribute='name').quote(schema=True, identifier=True) | upper | join(', ') -%}
 
     {%- if unique_key is not none -%}
-    merge into {{ target_relation }} target
-      using {{ tmp_relation }} temp
-      on (temp.{{ unique_key }} = target.{{ unique_key }})
+    merge into {{ target_relation.quote(schema=True, identifier=True) | upper }} target
+      using {{ tmp_relation.quote(schema=True, identifier=True) | upper }} temp
+      on (temp.{{ unique_key.quote(schema=True, identifier=True) | upper }} = target.{{ unique_key.quote(schema=True, identifier=True) | upper }})
     when matched then
       update set
       {% for col in dest_columns if col.name != unique_key %}
@@ -57,10 +57,10 @@
         {% endfor %}
       )
     {%- else %}
-    insert into {{ target_relation }} ({{ dest_cols_csv }})
+    insert into {{ target_relation.quote(schema=True, identifier=True) | upper }} ({{ dest_cols_csv }})
     (
        select {{ dest_cols_csv }}
-       from {{ tmp_relation }}
+       from {{ tmp_relation.quote(schema=True, identifier=True) | upper }}
     )
     {% endif %}
 {%- endmacro %}
