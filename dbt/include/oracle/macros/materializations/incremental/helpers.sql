87ab748d8dc805e6dbf373a4470454c3cbe9ee6d
Thomas Sapelza <thomas.sapelza@gknpm.com>
2022-05-12 08:58:12 +0200

    Use the correct variable in the loop

--- dbt/include/oracle/macros/materializations/incremental/helpers.sql
+++ dbt/include/oracle/macros/materializations/incremental/helpers.sql
@@ -40,32 +40,32 @@
 
 {% macro oracle_incremental_upsert(tmp_relation, target_relation, unique_key=none, statement_name="main") %}
     {%- set temp_columns = adapter.get_columns_in_relation(target_relation) | map(attribute='name') -%}
-    {{ log("### DEBUG oracle_incremental_upsert ### temp_columns" ~ temp_columns) }}
+    {{ log("### DEBUG oracle_incremental_upsert ### temp_columns " ~ temp_columns) }}
     {%- set dest_columns = [] -%}
-    {% for col in temp_columns  %}
-        {{ log("### DEBUG oracle_incremental_upsert ### for col" ~ col) }}
-        {{ dest_columns.append('"{}"'.format(col) | upper) }}
-        {{ log("### DEBUG oracle_incremental_upsert ### for col format" ~ ('"{}"'.format(col) | upper)) }}
+    {% for formatted_col in temp_columns  %}
+        {{ log("### DEBUG oracle_incremental_upsert ### for formatted_col " ~ formatted_col) }}
+        {{ dest_columns.append('"{}"'.format(formatted_col) | upper) }}
+        {{ log("### DEBUG oracle_incremental_upsert ### for formatted_col format " ~ ('"{}"'.format(formatted_col) | upper)) }}
     {% endfor %}
     {%- set dest_cols_csv = dest_columns | join(', ') -%}
-    {{ log("### DEBUG oracle_incremental_upsert ### dest_columns" ~ dest_columns) }}
-    {{ log("### DEBUG oracle_incremental_upsert ### dest_cols_csv" ~ dest_cols_csv) }}
+    {{ log("### DEBUG oracle_incremental_upsert ### dest_columns " ~ dest_columns) }}
+    {{ log("### DEBUG oracle_incremental_upsert ### dest_cols_csv " ~ dest_cols_csv) }}
 
     {%- if unique_key is not none -%}
     merge into {{ '"{}"'.format(target_relation) | upper }} target
       using {{ '"{}"'.format(tmp_relation) | upper }} temp
-      on (temp.{{ '"{}"'.format(unique_key) | upper }} = target.{{ '"{}"'.format(unique_key) | upper }})
+      on (temp.{{ unique_key }} = target.{{ unique_key }})
     when matched then
       update set
-      {% for col in dest_columns if col.name != unique_key %}
-        target.{{ col.name }} = temp.{{ col.name }}
+      {% for col in dest_columns if col != unique_key %}
+        target.{{ col }} = temp.{{ col }}
         {% if not loop.last %}, {% endif %}
       {% endfor %}
     when not matched then
       insert( {{ dest_cols_csv }} )
       values(
         {% for col in dest_columns %}
-          temp.{{ col.name }}
+          temp.{{ col }}
           {% if not loop.last %}, {% endif %}
         {% endfor %}
       )
